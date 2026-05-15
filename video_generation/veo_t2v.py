687bdadf7597c336c428a601dea76ab888605648
weiyi <weiyiboynextdoor@163.com>
2026-05-15 22:11:21 +0800

    use source parameter instead of deprecated

--- video_generation/veo_t2v.py
+++ video_generation/veo_t2v.py
@@ -1,21 +1,21 @@
+# https://docs.cloud.google.com/gemini-enterprise-agent-platform/models/video/generate-videos-from-text?hl=zh-cn
+
 import time
 from google import genai
-from google.genai.types import GenerateVideosConfig
+from google.genai.types import GenerateVideosSource, GenerateVideosConfig
 from dotenv import load_dotenv
 
 load_dotenv("../.env")
 
 client = genai.Client()
 
-# TODO(developer): Update and un-comment below line
-# output_gcs_uri = "gs://your-bucket/your-prefix"
-
 operation = client.models.generate_videos(
     model="veo-3.1-generate-001",
-    prompt="a cat reading a book",
+    source=GenerateVideosSource(
+        prompt="a cat reading a book",
+    ),
     config=GenerateVideosConfig(
         aspect_ratio="16:9",
-        # output_gcs_uri=output_gcs_uri,
     ),
 )
 
@@ -27,9 +27,5 @@ while not operation.done:
 if operation.response:
     # print(operation.result.generated_videos[0].video.uri)
     video = operation.response.generated_videos[0].video
-    if video.mime_type == "video/mp4":
-        with open("output.mp4", "wb") as f:
-            f.write(video.video_bytes)
-
-# Example response:
-# gs://your-bucket/your-prefix
+    with open("output.mp4", "wb") as f:
+        f.write(video.video_bytes)
